5f790945ff555c82bf882f429ba83ecfbf6c0900
Nice Watthayo <watthayo.warangkana@seattlecentral.edu>
2019-02-06 12:44:56 -0800

    Assignment 6 meeting and detail

--- pythonclubproj/club/urls.py
+++ pythonclubproj/club/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns=[
     path('', views.index, name='index'),
     path('resources/', views.resources, name='resources'),
+    path('getmeetings/', views.getmeetings, name='getmeetings'),
+    path('meetingdetail/<int:id>', views.meetingdetail, name='meetingdetail'),
 ]
--- pythonclubproj/club/views.py
+++ pythonclubproj/club/views.py
@@ -1,10 +1,18 @@
-from django.shortcuts import render
-from .models import Resource
+from django.shortcuts import render, get_object_or_404
+from .models import Resource, Meeting
 
 # Create your views here.
 def index (request):
     return render(request, 'club/index.html')
 
 def resources (request):
-    resource_list = Resource.objects.all
+    resource_list = Resource.objects.all()
     return render(request, 'club/resources.html', {'resource_list': resource_list})
+
+def getmeetings (request):
+    meeting_list = Meeting.objects.all()
+    return render (request, 'club/meetings.html', {'meeting_list': meeting_list})
+
+def meetingdetail(request, id):
+    detail=get_object_or_404(Meeting, pk=id)
+    return render(request, 'club/details.html',{'detail' : detail})
