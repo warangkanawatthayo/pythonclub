96e92be3104b90eaa32a59a6730f6b2566aab298
Nice Watthayo <watthayo.warangkana@seattlecentral.edu>
2019-02-20 12:30:07 -0800

    assignment8: forms

--- pythonclubproj/club/urls.py
+++ pythonclubproj/club/urls.py
@@ -6,4 +6,6 @@ urlpatterns=[
     path('resources/', views.resources, name='resources'),
     path('getmeetings/', views.getmeetings, name='getmeetings'),
     path('meetingdetail/<int:id>', views.meetingdetail, name='meetingdetail'),
+    path('newMeeting/', views.newMeeting, name='newmeeting'),
+    path('newResource/', views.newResource, name='newresource'),
 ]
--- pythonclubproj/club/views.py
+++ pythonclubproj/club/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Resource, Meeting
+from .forms import MeetingForm, ResourceForm
 
 # Create your views here.
 def index (request):
@@ -16,3 +17,28 @@ def getmeetings (request):
 def meetingdetail(request, id):
     detail=get_object_or_404(Meeting, pk=id)
     return render(request, 'club/details.html',{'detail' : detail})
+
+#form view
+def newMeeting(request):
+    form=MeetingForm
+    if request.method == 'POST':
+        form = MeetingForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit = True)
+            post.save()
+            form = MeetingForm()
+    else:
+        form = MeetingForm()
+    return render(request, 'club/newmeeting.html', {'form': form})
+
+def newResource(request):
+    form=ResourceForm
+    if request.method == 'POST':
+        form = ResourceForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit = True)
+            post.save()
+            form = ResourceForm()
+    else:
+        form = ResourceForm()
+    return render(request, 'club/newresource.html', {'form': form})
